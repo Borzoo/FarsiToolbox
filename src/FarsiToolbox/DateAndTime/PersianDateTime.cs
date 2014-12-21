f9171341905aebe73492dadd30211be155be91c4
brz <borzoo.esmailloo@gmail.com>
2014-12-22 01:19:44 +0330

    added PersianDateTime

---
+++ src/FarsiToolbox/DateAndTime/PersianDateTime.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Globalization;
+
+namespace FarsiToolbox.DateAndTime
+{
+    /// <summary>
+    /// Represents a Hijri Shamsi date
+    /// </summary>
+    public class PersianDateTime
+    {
+        /// <summary>
+        /// Gets the Hijri Shamsi Year
+        /// </summary>
+        public int Year { get; private set; }
+
+        /// <summary>
+        /// Gets the Hijri Shamsi Month
+        /// </summary>
+        public int Month { get; private set; }
+
+        /// <summary>
+        /// Gets the  Hijri Shamsi Day
+        /// </summary>
+        public int Day { get; private set; }
+
+        /// <summary>
+        /// Gets Hour
+        /// </summary>
+        public int Hour { get; private set; }
+
+        /// <summary>
+        /// Gets Minute
+        /// </summary>
+        public int Minute { get; private set; }
+
+        /// <summary>
+        /// Gets Second
+        /// </summary>
+        public int Second { get; private set; }
+
+        /// <summary>
+        /// Gets MilliSecond
+        /// </summary>
+        public int MilliSecond { get; private set; }
+
+        private static PersianCalendar _calendar = new PersianCalendar();
+
+        /// <summary>
+        /// Creates an instance of PersianDateTime
+        /// </summary>
+        /// <param name="year">Shamsi Year</param>
+        /// <param name="month">Shamsi Month</param>
+        /// <param name="day">Shamsi Day</param>
+        /// <param name="hour"></param>
+        /// <param name="minute"></param>
+        /// <param name="second"></param>
+        /// <param name="milliSecond"></param>
+        public PersianDateTime(int year, int month, int day, int hour = 0, int minute = 0, int second = 0, int milliSecond = 0)
+        {
+            Year = year;
+            Month = month;
+            Day = day;
+            Hour = hour;
+            Minute = minute;
+            Second = second;
+            MilliSecond = milliSecond;
+        }
+
+        /// <summary>
+        /// Creates an instance of PersianDateTime from an existing DateTimes
+        /// </summary>
+        /// <param name="dateTime"></param>
+        public PersianDateTime(DateTime dateTime)
+        {
+            Year = _calendar.GetYear(dateTime);
+            Month = _calendar.GetMonth(dateTime);
+            Day = _calendar.GetDayOfMonth(dateTime);
+            Hour = dateTime.Hour;
+            Minute = dateTime.Minute;
+            Second = dateTime.Second;
+            MilliSecond = dateTime.Millisecond;
+        }
+    }
+}
---
+++ src/FarsiToolboxTests/PersianDateTimeTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using FarsiToolbox.DateAndTime;
+using Xunit.Extensions;
+using Xunit;
+using System.Collections.Generic;
+
+namespace FarsiToolboxTests
+{
+    public class PersianDateTimeTests
+    {
+        public static IEnumerable<object[]> DateTimeConstructorData
+        {
+            get
+            {
+                yield return new object[] { new DateTime(2014, 12, 21, 10, 25, 11, 153), 1393, 9, 30, 10, 25, 11, 153 };
+                yield return new object[] { new DateTime(2015, 3, 20, 23, 59, 59, 0), 1393, 12, 29, 23, 59, 59, 0 };
+                yield return new object[] { new DateTime(2015, 3, 21, 10, 25, 11, 153), 1394, 1, 1, 10, 25, 11, 153 };
+                yield return new object[] { new DateTime(2017, 3, 20, 10, 25, 11, 153), 1395, 12, 30, 10, 25, 11, 153 };
+                yield return new object[] { new DateTime(2017, 3, 21, 10, 25, 11, 153), 1396, 1, 1, 10, 25, 11, 153 };
+            }
+        }
+
+        [Theory]
+        [PropertyData("DateTimeConstructorData")]
+        public void DateTimeConstructor(DateTime dateTime, int year, int month, int day, int hour, int minute, int second, int millisecond)
+        {
+            var persianDateTime = new PersianDateTime(dateTime);
+            Assert.Equal(persianDateTime.Year, year);
+            Assert.Equal(persianDateTime.Month, month);
+            Assert.Equal(persianDateTime.Day, day);
+            Assert.Equal(persianDateTime.Hour, hour);
+            Assert.Equal(persianDateTime.Minute, minute);
+            Assert.Equal(persianDateTime.Second, second);
+            Assert.Equal(persianDateTime.MilliSecond, millisecond);
+        }
+    }
+}
