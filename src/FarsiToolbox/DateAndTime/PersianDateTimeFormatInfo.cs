6fa4f99e0f6e5e2b09124c7d4653bef03f37a791
unknown <b.esmailloo@Borzoo-PC.septaco.org>
2014-12-25 15:40:23 +0330

    added string formatting to PersianDateTime

---
+++ src/FarsiToolbox/DateAndTime/PersianDateTimeFormatInfo.cs
@@ -0,0 +1,43 @@
+﻿using System.Globalization;
+
+namespace FarsiToolbox.DateAndTime
+{
+    internal static class PersianDateTimeFormatInfo
+    {
+        internal static DateTimeFormatInfo DateTimeFormatInfo { get; private set; }
+
+        /// <summary>
+        /// Format for 'G'
+        /// </summary>
+        internal static string GeneralLongTimePattern
+        {
+            get
+            {
+                return DateTimeFormatInfo.ShortDatePattern + " " + DateTimeFormatInfo.LongTimePattern;
+            }
+        }
+
+        /// <summary>
+        /// Format for 'g'
+        /// </summary>
+        internal static string GeneralShortTimePattern
+        {
+            get
+            {
+                return DateTimeFormatInfo.ShortDatePattern + " " + DateTimeFormatInfo.ShortTimePattern;
+            }
+        }
+
+        static PersianDateTimeFormatInfo()
+        {
+            var dtFormatInfo = DateTimeFormatInfo.GetInstance(new CultureInfo("fa"));
+            var monthNames = new[] { "فروردین", "اردیبهشت", "خرداد", "تیر", "مرداد", "شهریور", "مهر", "آبان", "آذر", "دی", "بهمن", "اسفند", "" };
+            dtFormatInfo.AbbreviatedMonthGenitiveNames = dtFormatInfo.MonthNames = monthNames;
+            dtFormatInfo.AbbreviatedMonthNames = dtFormatInfo.MonthGenitiveNames = monthNames;
+            var dayNames = new[] { "یکشنبه", "دوشنبه", "سه شنبه", "چهارشنبه", "پنج شنبه", "جمعه", "شنبه" };
+            dtFormatInfo.DayNames = dtFormatInfo.AbbreviatedDayNames = dayNames;
+            DateTimeFormatInfo = dtFormatInfo;
+        }
+    }
+
+}
